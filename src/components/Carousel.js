9989b30409f6c197acb4fbf9497bb7efa4d77ac2
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-11-15 15:15:37 -0700

    Updated Portfolio section

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,7 +3,11 @@ import React from 'react';
 import Card from '../components/Card';
 
 import studybuddy from '../assets/images/studybuddy.png';
+import quiz from '../assets/images/quiz.png';
+import employeetracker from '../assets/images/employeetracker.png';
 import eventsmash from '../assets/images/EventSmash.png';
+import Burger from '../assets/images/burger.png';
+import weatherdashboard from '../assets/images/weatherdashboard.png';
 import TemplateEngine from '../assets/images/templateengine.png';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -32,10 +36,42 @@ class Carousel extends React.Component {
                 },
                 {
                     id: 2,
+                    title: 'JS Code Quiz',
+                    subTitle: 'How is Your JavaScript Knowledge?',
+                    imgSrc: quiz,
+                    link: 'https://github.com/alsricha/Code-Quiz',
+                    selected: false
+                },
+                {
+                    id: 3,
                     title: 'Template Engine',
-                    subTitle: 'A social network for developers',
+                    subTitle: '',
                     imgSrc: TemplateEngine,
                     link: 'https://github.com/alsricha/wk10-Template-Engine',
+                    selected: false
+                },
+                {
+                    id: 4,
+                    title: 'Weather Dashboard',
+                    subTitle: 'Daily Weather Application',
+                    imgSrc: weatherdashboard,
+                    link: 'https://github.com/alsricha/Weather-Dashboard',
+                    selected: false
+                },
+                {
+                    id: 5,
+                    title: 'Employee Tracker',
+                    subTitle: 'Application to Track Employees',
+                    imgSrc: employeetracker,
+                    link: 'https://github.com/alsricha/Employee-Tracker',
+                    selected: false
+                },
+                {
+                    id: 6,
+                    title: 'Eat-da-Burger',
+                    subTitle: 'A burger logger using MySQL, Node, Handlebars and More!',
+                    imgSrc: Burger,
+                    link: 'https://github.com/alsricha/Burger',
                     selected: false
                 },
             ]
