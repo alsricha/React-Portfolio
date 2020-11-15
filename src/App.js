ff8dc5d0b4f153f67afec84d31b44834a9e991fe
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-11-15 12:43:31 -0700

    Update to App.js imports

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import './App.css';
 
 import Footer from './components/Footer';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
+import HomePage from './Pages/HomePage';
+import AboutPage from './Pages/AboutPage';
+import ContactPage from './Pages/ContactPage';
+
 
 class App extends React.Component {
-
+  
   constructor(props) {
     super(props);
     this.state = {
-      title: 'Alex Skudrovskis',
+      title: 'Garrett Love',
       headerLinks: [
-        { title: 'Home', path: '/'},
-        { title: 'About', path: '/about'},
-        { title: 'Contact', path: '/contact'},
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: 'Contact', path: '/contact' }
       ],
       home: {
-        title: 'Studen Develper',
-        subTitle: 'Projects from Denver University',
+        title: 'DU Student',
+        subTitle: 'Denver University BootCamp',
         text: 'Checkout my projects below'
       },
-
       about: {
         title: 'About Me'
       },
-
       contact: {
         title: 'Let\'s Connect'
       }
@@ -40,33 +39,31 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-        <Container className="p-0" fluid= {true}>
+        <Container className="p-0" fluid={true}>
           
-          <Navbar className = "border-bottom" bg="transparent" expand="lg">
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
             <Navbar.Brand>Alex Skudrovskis</Navbar.Brand>
 
-            <Navbar.Toggle className ="border-0" aria-controls="navbar-toggle"/>
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
             <Navbar.Collapse id="navbar-toggle">
-              <Nav className ="ml-auto">
-                <Link className="nav-link"to= "/"> Home</Link>
-                <Link className="nav-link"to= "/about"> About</Link>
-                <Link className="nav-link"to= "/contact"> Contact</Link>
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
 
           <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
-
           <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
-
           <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
-
+          
           <Footer />
+
         </Container>
       </Router>
-      );
+    );
   }
-
 }
 
 export default App;
