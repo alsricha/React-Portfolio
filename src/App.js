4cba00466fa42939c31440436db8fa1c25791181
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-11-29 12:14:02 -0700

    Add React switch to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Link,Route } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -53,10 +53,11 @@ class App extends React.Component {
               </Nav>
             </Navbar.Collapse>
           </Navbar>
-
+          <Switch>
           <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
           <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
           <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
+          </Switch>
           
           <Footer />
 
